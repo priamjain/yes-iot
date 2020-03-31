63c33b3926f149d51dbb15f78e73b0b82f7c2e0e
Priam Jain <priamjain8@gmail.com>
2020-03-31 18:52:46 +0530

    Data send to arduino

--- python-scripts/Data2arduino.py
+++ python-scripts/Data2arduino.py
@@ -1,9 +1,7 @@
 import serial
 from firebase import firebase
-#from firebase_admin import credentials
-#from firebase_admin import db
-#from firebase import firebase
+arduino = serial.Serial('/dev/ttyACM0',9600);
 firebase = firebase.FirebaseApplication('https://yes-iot.firebaseio.com')
-result = firebase.get('/User1/D1', None)
-print(result)
-#default_app = firebase_admin.initialize_app()
+while(1):
+    result = firebase.get('/User1/D1', None)
+    arduino.write(result.encode())
