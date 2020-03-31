82aa3b9e8418a8fac0eaf99d8a09d233cb90c21d
Priam Jain <priamjain8@gmail.com>
2020-03-31 19:53:29 +0530

    Final changes in arguino code

--- arduino-uno-source-code/Blink/Blink.ino
+++ arduino-uno-source-code/Blink/Blink.ino
@@ -1,13 +1,13 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   Serial.begin(9600);
-  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
 }
 void loop() {
  if(Serial.available()){
   int x =Serial.read();
   Serial.println(x);
-  if(x==49)digitalWrite(13,HIGH);
-  else digitalWrite(13,LOW); 
+  if(x==49)digitalWrite(12,HIGH);
+  else digitalWrite(12,LOW); 
  }
  }
