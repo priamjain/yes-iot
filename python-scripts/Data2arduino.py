e70eb2125ecb060c706efe39dc563137f4086d42
Priam Jain <priamjain8@gmail.com>
2020-03-31 17:18:53 +0530

    Data sent to arduino using serial

--- python-scripts/Data2arduino.py
+++ python-scripts/Data2arduino.py
@@ -1,15 +1,17 @@
 import serial
 import time
+#import os
 from flask import Flask
 from flask import request
 from flask import render_template
 arduino = serial.Serial('/dev/ttyACM1',9600);
 time.sleep(2)
 app = Flask(__name__)
-
+path = os.getcwd() 
+#indfile = os.path.abspath(os.path.join(path, os.pardir))
 @app.route('/')
 def my_form():
-    return render_template("my_form.html")
+    return render_template("index.html")
 
 @app.route('/', methods=['POST'])
 def my_form_post():
