5ad3f486006855a6d95e3c390d17878431bd4151
Priam Jain <priamjain8@gmail.com>
2020-03-31 18:58:57 +0530

    LED Controlled using arduino

--- arduino-uno-source-code/Blink/Blink.ino
+++ arduino-uno-source-code/Blink/Blink.ino
@@ -1,10 +1,12 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
 }
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
-}p
+ if(Serial.available()){
+  int x =Serial.read();
+  if(x==1)digitalWrite(13,HIGH);
+  else digitalWrite(13,LOW); 
+ }
+ }
