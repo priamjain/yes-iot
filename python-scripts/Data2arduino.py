4779fe39c2a7eb1775d84c4abc59f5cedc325747
Priam Jain <priamjain8@gmail.com>
2020-03-31 18:33:34 +0530

    Firebase Data to Python3

--- python-scripts/Data2arduino.py
+++ python-scripts/Data2arduino.py
@@ -1,24 +1,9 @@
 import serial
-import time
-#import os
-from flask import Flask
-from flask import request
-from flask import render_template
-arduino = serial.Serial('/dev/ttyACM1',9600);
-time.sleep(2)
-app = Flask(__name__)
-path = os.getcwd() 
-#indfile = os.path.abspath(os.path.join(path, os.pardir))
-@app.route('/')
-def my_form():
-    return render_template("index.html")
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form.getlist('cb')
-    str1 = ''.join([str(elem) for elem in text])
-    arduino.write(str1.encode())
-    print(str1)
-    return str1
-if __name__ == '__main__':
-    app.run()
+from firebase import firebase
+#from firebase_admin import credentials
+#from firebase_admin import db
+#from firebase import firebase
+firebase = firebase.FirebaseApplication('https://yes-iot.firebaseio.com')
+result = firebase.get('/User1/D1', None)
+print(result)
+#default_app = firebase_admin.initialize_app()
